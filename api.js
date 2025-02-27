c8ae3b0ae71d1edc7f309252efa317f25c9569d6
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-27 09:37:39 -0500

    Update api.js

--- api.js
+++ api.js
@@ -3,13 +3,12 @@ let connectedIP = 'No IP connected';
 exports.handler = async (event, context) => {
     if (event.httpMethod === 'POST') {
         const body = JSON.parse(event.body);
-        connectedIP = body.ip; // Store the IP sent from Python script
+        connectedIP = body.ip;
         return {
             statusCode: 200,
             body: JSON.stringify({ message: `IP updated to ${connectedIP}` }),
         };
     } else {
-        // GET request
         return {
             statusCode: 200,
             body: JSON.stringify({ message: `Connected to ${connectedIP}` }),
