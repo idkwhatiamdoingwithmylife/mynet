f265f7ece90ec82e111cfeb7dd7bc755a48acfc6
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-27 08:50:14 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,3 +1,4 @@
 export default function handler(req, res) {
-    res.status(200).json({ message: 'Hi! Computer connected' });
+  const computerName = req.body.computerName || 'Unknown';
+  res.status(200).json({ message: `Connected to ${computerName}` });
 }
