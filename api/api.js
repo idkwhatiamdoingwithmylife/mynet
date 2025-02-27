89262877396882c1caebd7d1fe094001c042e57e
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-27 10:04:10 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,18 +1,10 @@
 module.exports = async (req, res) => {
-  const message = req.query.message;
+    const encodedMessage = req.query.message;
 
-  if (message) {
-    const parts = message.split(", ");
-    if (parts.length === 2 && parts[0].startsWith('Visited:') && parts[1].startsWith('MAC:')) {
-      const ipAddress = parts[0].split(": ")[1];
-      const macAddress = parts[1].split(": ")[1];
-
-      // Respond with the connected IP address and MAC address
-      res.status(200).json({ message: `Connected to ${ipAddress}, MAC Address: ${macAddress}` });
+    if (encodedMessage) {
+        const deviceName = encodedMessage.split(': ')[1];
+        res.status(200).json({ message: deviceName });
     } else {
-      res.status(400).json({ error: 'Invalid message format' });
+        res.status(400).json({ error: 'No message provided' });
     }
-  } else {
-    res.status(400).json({ error: 'No message provided' });
-  }
 };
