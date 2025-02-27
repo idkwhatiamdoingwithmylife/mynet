2e1b273b6fac4da2aca1fb165f45217e130659b6
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-27 10:08:17 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,10 +1,12 @@
 module.exports = async (req, res) => {
     const encodedMessage = req.query.message;
 
+    let htmlResponse = '<h1>Connection Status</h1><div id="status">Not Connected</div>';
+
     if (encodedMessage) {
         const deviceName = encodedMessage.split(': ')[1];
-        res.status(200).json({ message: deviceName });
-    } else {
-        res.status(400).json({ error: 'No message provided' });
+        htmlResponse = `<h1>Connection Status</h1><div id="status">Connected to: ${deviceName}</div>`;
     }
+
+    res.status(200).send(htmlResponse);
 };
