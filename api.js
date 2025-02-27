dae149159053b5209652a864c3559ce5310d34e8
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-27 09:38:17 -0500

    Update api.js

--- api.js
+++ api.js
@@ -1,16 +1,23 @@
 let connectedIP = 'No IP connected';
 
 exports.handler = async (event, context) => {
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+    };
+
     if (event.httpMethod === 'POST') {
         const body = JSON.parse(event.body);
         connectedIP = body.ip;
         return {
             statusCode: 200,
+            headers,
             body: JSON.stringify({ message: `IP updated to ${connectedIP}` }),
         };
     } else {
         return {
             statusCode: 200,
+            headers,
             body: JSON.stringify({ message: `Connected to ${connectedIP}` }),
         };
     }
