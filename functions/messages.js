43ed86da16722e4381fccc781efd89c57e89af4e
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-27 10:42:20 -0500

    Create messages.js

---
+++ functions/messages.js
@@ -0,0 +1,19 @@
+let messages = []; // In-memory storage for messages
+
+exports.handler = async (event) => {
+    if (event.httpMethod === 'GET') {
+        // Return all messages
+        return {
+            statusCode: 200,
+            body: JSON.stringify(messages),
+        };
+    } else if (event.httpMethod === 'POST') {
+        const { username, message } = JSON.parse(event.body);
+        if (username && message) {
+            messages.push({ username, message });
+            return { statusCode: 201, body: '' };
+        }
+        return { statusCode: 400, body: 'Bad Request' };
+    }
+    return { statusCode: 405, body: 'Method Not Allowed' };
+};
