4aacd57f046ce2091cca7682488e3d2e161f66ab
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-27 08:46:45 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,8 +1,3 @@
-// api/api.js
-export default async (req, res) => {
-  if (req.method === 'POST') {
-    res.status(200).json({ message: 'Hi, computer connected!' });
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-};
+export default function handler(req, res) {
+    res.status(200).json({ message: 'Hi! Computer connected' });
+}
