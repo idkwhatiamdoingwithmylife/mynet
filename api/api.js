23d0f7bac82d897596e7170639bc4c6fac30fc78
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-27 09:54:15 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -2,11 +2,13 @@ module.exports = async (req, res) => {
   const message = req.query.message;
 
   if (message) {
-    if (message.startsWith('Visited:')) {
-      const fullUrl = message.split(": ")[1];
-      
-      // Respond with the connected IP address
-      res.status(200).json({ message: `Connected to ${fullUrl}` });
+    const parts = message.split(", ");
+    if (parts.length === 2 && parts[0].startsWith('Visited:') && parts[1].startsWith('MAC:')) {
+      const ipAddress = parts[0].split(": ")[1];
+      const macAddress = parts[1].split(": ")[1];
+
+      // Respond with the connected IP address and MAC address
+      res.status(200).json({ message: `Connected to ${ipAddress}, MAC Address: ${macAddress}` });
     } else {
       res.status(400).json({ error: 'Invalid message format' });
     }
