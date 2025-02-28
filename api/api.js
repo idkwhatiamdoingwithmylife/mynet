4a939931eaa93ee73e297e7381f7c13fdcbca6d2
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-27 21:58:23 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -2,15 +2,23 @@ let list = [];  // In-memory list to store the data (note: this resets every tim
 
 exports.handler = async function(event, context) {
     if (event.httpMethod === 'POST') {
-        const body = JSON.parse(event.body);
-
+        const body = JSON.parse(event.body);  // Parse the request body
+        
         if (body.action === 'add') {
             // Add "hi" to the list
             list.push('hi');
         }
+
+        if (body.action === 'remove') {
+            // Remove the last "hi" from the list
+            const index = list.lastIndexOf('hi');
+            if (index !== -1) {
+                list.splice(index, 1);  // Remove the element from the list
+            }
+        }
     }
 
-    // Respond with the current list
+    // Respond with the current list after any modifications
     return {
         statusCode: 200,
         body: JSON.stringify({ list: list })
