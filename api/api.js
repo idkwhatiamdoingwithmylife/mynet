ba4aa0d8bbde1b6f53872cfd6c50fe4047d03ec5
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-28 13:27:12 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -10,13 +10,19 @@ exports.handler = async function(event, context) {
         const body = JSON.parse(event.body);
 
         if (body.action === 'sendMessage' && body.message && body.color) {
+            let username = body.username.trim();
+            if (!username) {
+                username = "Guest" + Math.floor(10000 + Math.random() * 90000);
+            }
+
             messages.push({
+                username: username,
                 message: body.message,
                 color: body.color
             });
-        }
 
-        process.env[messagesKey] = JSON.stringify(messages);
+            process.env[messagesKey] = JSON.stringify(messages);
+        }
     }
 
     return {
