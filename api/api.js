7635fa77ac92ff5cb151981565a92284d78a0465
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-27 21:21:30 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,21 +1,31 @@
 let items = [];
 
 exports.handler = async (event, context) => {
+    // Add CORS headers to allow requests from other domains
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Content-Type': 'application/json',
+    };
+
     if (event.httpMethod === 'POST') {
         const { item } = JSON.parse(event.body);
         items.push(item);
         return {
             statusCode: 200,
+            headers,
             body: JSON.stringify({ message: 'Item added successfully' }),
         };
     } else if (event.httpMethod === 'GET') {
         return {
             statusCode: 200,
+            headers,
             body: JSON.stringify(items),
         };
     } else {
         return {
             statusCode: 405,
+            headers,
             body: JSON.stringify({ message: 'Method Not Allowed' }),
         };
     }
