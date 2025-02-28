525195a7ad876cfc5ff6d16265b141412c601b0c
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-28 10:04:06 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -14,36 +14,29 @@ exports.handler = async function(event, context) {
     if (event.httpMethod === 'POST') {
         const body = JSON.parse(event.body);
 
-        if (body.action === 'createAccount' && body.username && body.color) {
+        if (body.action === 'createAccount' && body.username && body.password && body.color) {
             if (users[body.username]) {
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({ success: false })
-                };
+                return { statusCode: 200, body: JSON.stringify({ success: false }) };
             }
 
-            users[body.username] = { color: body.color };
+            users[body.username] = { password: body.password, color: body.color };
             process.env[usersKey] = JSON.stringify(users);
 
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ success: true })
-            };
+            return { statusCode: 200, body: JSON.stringify({ success: true }) };
+        }
+
+        if (body.action === 'login' && body.username && body.password) {
+            if (users[body.username] && users[body.username].password === body.password) {
+                return { statusCode: 200, body: JSON.stringify({ success: true, color: users[body.username].color }) };
+            }
+            return { statusCode: 200, body: JSON.stringify({ success: false }) };
         }
 
         if (body.action === 'sendMessage' && body.username && body.message && users[body.username]) {
-            messages.push({
-                username: body.username,
-                message: body.message,
-                color: users[body.username].color
-            });
-
+            messages.push({ username: body.username, message: body.message, color: users[body.username].color });
             process.env[messagesKey] = JSON.stringify(messages);
         }
     }
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ list: messages })
-    };
+    return { statusCode: 200, body: JSON.stringify({ list: messages }) };
 };
