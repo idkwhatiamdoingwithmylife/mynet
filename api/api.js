07fc04d4171ae13900a91b96101a005f5fd97a0c
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-02-28 10:01:33 -0500

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,22 +1,45 @@
 let messages = [];
+let users = {};
 let messagesKey = 'CHAT_MESSAGES';
+let usersKey = 'CHAT_USERS';
 
 exports.handler = async function(event, context) {
     if (process.env[messagesKey]) {
         messages = JSON.parse(process.env[messagesKey]);
     }
+    if (process.env[usersKey]) {
+        users = JSON.parse(process.env[usersKey]);
+    }
 
     if (event.httpMethod === 'POST') {
         const body = JSON.parse(event.body);
 
-        if (body.action === 'sendMessage' && body.message && body.color) {
-            messages.push({
-                message: body.message,
-                color: body.color
-            });
+        if (body.action === 'createAccount' && body.username && body.color) {
+            if (users[body.username]) {
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ success: false })
+                };
+            }
+
+            users[body.username] = { color: body.color };
+            process.env[usersKey] = JSON.stringify(users);
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ success: true })
+            };
         }
 
-        process.env[messagesKey] = JSON.stringify(messages);
+        if (body.action === 'sendMessage' && body.username && body.message && users[body.username]) {
+            messages.push({
+                username: body.username,
+                message: body.message,
+                color: users[body.username].color
+            });
+
+            process.env[messagesKey] = JSON.stringify(messages);
+        }
     }
 
     return {
